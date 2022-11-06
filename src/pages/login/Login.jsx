c4fae50197e586560d4d934380a1d5dde3a5441c
Sinan <sinankzu@gmail.com>
2022-11-06 17:26:58 +0100

    v3

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,4 @@
-import React,{useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
 import {
   FormContainer,
   Header,
@@ -10,13 +9,16 @@ import {
   StyledInput,
 } from "./LoginStyles";
 import mealSvg from "../../assets/meal.svg";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-const[nickName, setNickname]=useState();
+  const [nickName, setNickname] = useState();
+  const navigate = useNavigate();
 
-const navigate = useNavigate();
-
-
-
+  const backendeYolla = (e) => {
+    e.preventDefault();
+    // localStorage.setItem("kullanici", JSON.stringify(nickName));
+    navigate("/home");
+  };
 
   return (
     <LoginContainer>
@@ -25,13 +27,17 @@ const navigate = useNavigate();
         <Header>{"<Clarusway/>"}Recipe</Header>
         {/* login sayfasındaki yuvarlak olayın içindeki yazı (Header) */}
 
-        <StyledForm  onSubmit={()=>navigate("/home")}>
+        <StyledForm onSubmit={backendeYolla}>
           {/* 3 kutunun olduğu form  */}
-          <StyledInput type="text" placeholder="username" required onChange={(e)=>setNickname(e.target.value)}/>
-
+          <StyledInput
+            type="text"
+            placeholder="username"
+            required
+            onChange={(e) => setNickname(e.target.value)}
+          />
 
           <StyledInput type="password" placeholder="password" required />
-          <StyledButton type="submit" >Login</StyledButton>
+          <StyledButton type="submit">Login</StyledButton>
         </StyledForm>
       </FormContainer>
     </LoginContainer>
